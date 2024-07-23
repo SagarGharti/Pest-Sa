a6395403fa0372447d6719ade2f20dfb08e59953
barsha poudel <barshapoudelko@gmail.com>
2024-07-23 21:01:15 +0545

    fixing: UI

--- pages/api/sendMessage.js
+++ pages/api/sendMessage.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     const whatsappMessage = `Name: ${name}%0APhone: ${phone}%0AMessage: ${message}`;
     
     // Replace this with the admin's WhatsApp number (include country code)
-    const adminWhatsAppNumber = '9779868338755';
+    const adminWhatsAppNumber = '9868338755';
 
     // Create the WhatsApp click to chat link
     const whatsappLink = `https://wa.me/${adminWhatsAppNumber}?text=${whatsappMessage}`;
